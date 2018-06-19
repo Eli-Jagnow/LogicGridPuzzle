77da3b416559768c1adbfba52d8bd3a515ad1fe7
Dan Jagnow <dan.jagnow@gmail.com>
2018-06-18 23:16:58 -0500

    Stubbed in a puzzle page
    Stubbed in a puzzle page with a very basic layout.  Added it to the menu.

---
+++ LogicGridPuzzle.Web/Controllers/PuzzleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LogicGridPuzzle.Web.Controllers
+{
+    public class PuzzleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
